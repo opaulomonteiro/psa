4b3eaac3dc6f129596296112c13975733bd9fe60
PAULO VICENTE CESTARI MONTEIRO <11105929@pucrs.br>
2017-09-06 20:32:42 -0300

    Esqueleto do trabalho

---
+++ ExercicioCodeFirst-Cast/ExercicioCodeFirst-Cast/PL.Models/IMovieDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public interface IMovieDAO : IDisposable
+    {
+        IEnumerable<Movie> GetMovies();
+        Movie GetMovieByID(int movieId);
+        void Add(Movie movie);
+        void Delete(int movieId);
+        void Update(Movie movie);
+    }
+}
